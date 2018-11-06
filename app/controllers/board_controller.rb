a2e147af744d00b046cde8a84b4eb62716e4cfa3
Hyunju <gus7wn@gmail.com>
2018-11-06 03:10:17 +0000

    게시판

---
+++ app/controllers/board_controller.rb
@@ -0,0 +1,62 @@
+class BoardController < ApplicationController
+  def index
+    #포스팅 목록 보여주기
+    @post_all = Post.all
+  end
+ 
+  def show
+    #제목 클릭하면 포스팅 보여주기
+    post_id = params[:id]
+    @post = Post.find(post_id)
+    
+  end
+
+  def new
+    #새로운 포스팅 작성하기
+    
+  end
+
+  def create
+    #포스팅 저장하기
+    
+    # Parameters: {"title"=>"ss", "editor"=>"ss", "content"=>"sss"}
+    @title = params[:title]
+    @editor = params[:editor]
+    @content = params[:content]
+    
+    new_post = Post.new
+    new_post.title = @title
+    new_post.editor = @editor
+    new_post.content = @content
+    new_post.save
+    
+    redirect_to "/"
+  end
+
+  def edit
+    #수정하여 저장하기
+    post_id=params[:id]
+    @post = Post.find(post_id)
+  end
+
+  def update
+    #변경된 내용 DB저장
+    edit_post = Post.find(params[:id])
+    edit_post.title = params[:title]
+    edit_post.editor = params[:editor]
+    edit_post.content = params[:content]
+    edit_post.save
+    
+    redirect_to "/"
+    
+  end
+
+  def delete
+    #포스팅 삭제하기
+    post_id= params[:id]
+    delete_post = Post.find(post_id)
+    delete_post.destroy
+    
+    redirect_to "/"
+  end
+end
